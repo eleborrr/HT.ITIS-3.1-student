a18beaf003746a64d3f1e5217a0e501d27eaeb57
Gleb Petukhov <gleb-petuhov@mail.ru>
2023-12-01 17:15:47 +0300

    tests passing

---
+++ Dotnet.Homeworks.Mediator/MyMediator.cs
@@ -0,0 +1,19 @@
+﻿namespace Dotnet.Homeworks.Mediator;
+
+public class MyMediator: IMediator
+{
+    public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Send<TRequest>(TRequest request, CancellationToken cancellationToken = default) where TRequest : IRequest
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<dynamic?> Send(dynamic request, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
