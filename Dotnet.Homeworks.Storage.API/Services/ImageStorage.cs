c89cbe6c201d4e55492e176bdbc3f2b13468807f
Gleb Petukhov <gleb-petuhov@mail.ru>
2023-12-19 17:26:45 +0300

    Image storage + storage factory

--- Dotnet.Homeworks.Storage.API/Services/ImageStorage.cs
+++ Dotnet.Homeworks.Storage.API/Services/ImageStorage.cs
@@ -1,33 +1,105 @@
-﻿using Dotnet.Homeworks.Shared.Dto;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Dotnet.Homeworks.Shared.Dto;
 using Dotnet.Homeworks.Storage.API.Dto.Internal;
+using Minio;
+using Minio.DataModel.Args;
 
 namespace Dotnet.Homeworks.Storage.API.Services;
 
 public class ImageStorage : IStorage<Image>
 {
-    public Task<Result> PutItemAsync(Image item, CancellationToken cancellationToken = default)
+    private readonly string _bucketStorageName;
+    private readonly IMinioClient _minioClient;
+
+    public ImageStorage(string bucketStorageName, IMinioClient minioClient)
     {
-        throw new NotImplementedException();
+        _bucketStorageName = bucketStorageName;
+        _minioClient = minioClient;
     }
 
-    public Task<Image?> GetItemAsync(string itemName, CancellationToken cancellationToken = default)
+    public async Task<Result> PutItemAsync(Image item, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var putObjArgs = new PutObjectArgs()
+                .WithBucket(_bucketStorageName)
+                .WithObject(item.FileName)
+                .WithStreamData(item.Content)
+                .WithContentType(item.ContentType)
+                .WithObjectSize(item.Content.Length);
+            await _minioClient.PutObjectAsync(putObjArgs, cancellationToken);
+            return new Result(true);
+        }
+        catch (Exception e)
+        {
+            return new Result(false, e.Message);
+        }
+    }
+
+    public async Task<Image?> GetItemAsync(string itemName, CancellationToken cancellationToken = default)
+    {
+        var memoryStream = new MemoryStream();   
+
+        var putObjArgs = new GetObjectArgs()
+            .WithBucket(_bucketStorageName)
+            .WithObject(itemName)
+            .WithCallbackStream(stream =>
+            {
+                stream.CopyTo(memoryStream);
+            });
+        await _minioClient.GetObjectAsync(putObjArgs, cancellationToken);
+        return JsonSerializer.Deserialize<Image>(memoryStream);
     }
 
     public Task<Result> RemoveItemAsync(string itemName, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var removeObjArgs = new RemoveObjectArgs()
+                .WithBucket(_bucketStorageName)
+                .WithObject(itemName);
+            _minioClient.RemoveObjectAsync(removeObjArgs, cancellationToken);
+            return Task.FromResult(new Result(true));
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new Result(false, e.Message));
+        }
     }
 
     public Task<IEnumerable<string>> EnumerateItemNamesAsync(CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var res = new List<string>();
+        var listArgs = new ListObjectsArgs()
+            .WithBucket(_bucketStorageName);
+        var observable = _minioClient.ListObjectsAsync(listArgs);
+        var subscription = observable.Subscribe(
+                item => res.Add(item.Key),
+            ex => Console.WriteLine($"OnError: {ex}"),
+            () => Console.WriteLine($"Listed all objects in bucket {_bucketStorageName}\n"));
+        return Task.FromResult(res.AsEnumerable());
     }
 
     public Task<Result> CopyItemToBucketAsync(string itemName, string destinationBucketName,
         CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var objSourceArgs = new CopySourceObjectArgs()
+                .WithBucket(_bucketStorageName)
+                .WithObject(itemName);
+
+            var copyObjArgs = new CopyObjectArgs()
+                .WithBucket(destinationBucketName)
+                .WithCopyObjectSource(objSourceArgs);
+
+            _minioClient.CopyObjectAsync(copyObjArgs, cancellationToken);
+            return Task.FromResult(new Result(true));
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new Result(false, e.Message));
+        }
     }
 }
--- Dotnet.Homeworks.Storage.API/ServicesExtensions/AddMinioExtensions.cs
+++ Dotnet.Homeworks.Storage.API/ServicesExtensions/AddMinioExtensions.cs
@@ -1,4 +1,6 @@
-using Dotnet.Homeworks.Storage.API.Configuration;
+using Minio;
+using Minio.DataModel.Args;
+using MinioConfig = Dotnet.Homeworks.Storage.API.Configuration.MinioConfig;
 
 namespace Dotnet.Homeworks.Storage.API.ServicesExtensions;
 
@@ -7,6 +9,13 @@ public static class AddMinioExtensions
     public static IServiceCollection AddMinioClient(this IServiceCollection services,
         MinioConfig minioConfiguration)
     {
-        throw new NotImplementedException();
+        var minioClient = new MinioClient().WithEndpoint(minioConfiguration.Endpoint)
+            .WithCredentials(minioConfiguration.Username, minioConfiguration.Password)
+            .Build();
+
+        
+        
+        services.AddSingleton<IMinioClient>(o => minioClient);
+        return services;
     }
 }
